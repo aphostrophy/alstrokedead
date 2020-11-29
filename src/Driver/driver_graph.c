20117c1e5904f228142cf306ec64abd838f1bcbe
Rafidika <rafidika2013@gmail.com>
2020-11-29 20:07:10 +0700

    graf lagi anjing

--- src/Driver/driver_graph.c
+++ src/Driver/driver_graph.c
@@ -6,9 +6,17 @@ int main() {
     Graph G;
     BacaGraph(&G,"../file/graph.txt");
     if (Graph_First(G) != Nil) {
-        printf("Berhasil!");
+        printf("Berhasil!\n");
+        adrPeta P = Graph_First(G);
+        while (P != Nil) {
+            printf("%d ", idPeta(P).map);
+            printf("%d ", Absis(idPeta(P).p));
+            printf("%d ", Ordinat(idPeta(P).p));
+            printf("\n");
+            P = Graph_Next(P);
+        }
     }
     else {
-        printf("lol no");
+        printf("lol no\n");
     }
 }
