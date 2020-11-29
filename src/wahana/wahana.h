6851841b9fcdb72f61eb84bca1eaef7eacaa4d07
DenilsenAxel <denilsenaxel@gmail.com>
2020-11-29 13:29:37 -0500

    komentarin wahana

--- src/wahana/wahana.h
+++ src/wahana/wahana.h
@@ -33,7 +33,8 @@ typedef struct {
 void bacaWahana(Wahana *W, char* namaFile);
 /* Fungsi untuk mengisi list Wahana W dengan data wahana */
 
-/* SELEKTOR */
+/* *** Selektor - Selektor Penting *** */
+/* Selektor properti wahana, representatif sesuai namanya */
 Kata getNama(Wahana *W, char id);
 int getHarga(Wahana *W, char id);
 int getDurasi(Wahana *W, char id);
@@ -45,17 +46,33 @@ int getPengunjung(Wahana *W, char id);
 int getPenghasilan(Wahana *W, char id);
 int getTotalPengunjung(Wahana *W, char id);
 int getTotalPenghasilan(Wahana *W, char id);
+
+/* *** Primitif - Primitif Penting *** */
 void printListWahana(Wahana *W);
+/* Prosedur untuk mencetak list wahana beserta id nya */
 void printDetailWahana(Wahana *W, char id, ListNode **head);
+/* Prosedur untuk mencetak detail wahana berdasarkan char id nya, 
+   menggunakan linked list untuk mencetak history upgradenya */
 void printReportWahana(Wahana *W, char id);
+/* Prosedur untuk mencetak laporan wahana berupa pengunjung dan penghasilan wahana hari ini,
+   serta total pengunjung dan penghasilan wahana selama bermain*/
 void printNotBuilded(Wahana *W);
+/* Prosedur untuk mencetak list wahana yang belum dibangun */
 InfoWahana getWahanabyID(Wahana *W,char id);
+/* Fungsi yang mengembalikan info wahana dengan char id yang bersangkutan */
 int GetIndex(Wahana *W, char id);
+/* Fungsi yang mengembalikan indeks dari wahana dengan char id */
 int countBrokenWahana(Wahana *W);
+/* Fungsi yang mengembalikan jumlah wahana yang sedang rusak */
 void printBrokenWahana(Wahana *W);
-InfoWahana getWahanabyID(Wahana *W,char id);
+/* Prosedur untuk menetak list wahana yang sedang rusak */
 boolean IsWahanaBenar(Kata W, Wahana LW);
+/* Fungsi yang mengembalikan true jika W terdapat dalam list wahana LW */
 boolean IsAdaWahanaGood(Wahana W);
+/* Fungsi yang mengembalikan true jika W berada dalam kondisi baik dan tidak rusak */
 void SetZero(Wahana *W);
+/* Prosedur untuk mengubah jumlah penghasilan dan pengunjung tiap wahana menjadi 0 setiap hari baru
+   I.S setiap jumlah pengunjung dan penghasilan harian wahana bebas, bisa 0 atau sembarang int 
+   F.S setiap jumlah pengunjung dan penghasilan harian wahana menjadi 0 */
 
 #endif
